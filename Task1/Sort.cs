df935e974b16f34e7f47a696c8e4e856234fb3ad
AlexKarmaz <karmazct@mail.ru>
2017-03-12 20:19:56 +0300

    Add XML documentation comments

--- Task1/Sort.cs
+++ Task1/Sort.cs
@@ -3,8 +3,20 @@
 
 namespace Task1
 {
+    /// <summary>
+    /// This class contains methods that implement the merge sorting of an integer array 
+    /// </summary>
     public static class Sort
     {
+        /// <summary>
+        /// Copies a specific number of elements from an System.Array starting at the specified source index 
+        /// </summary>
+        /// <param name="data">The System.Array that contains the data to copy</param>
+        /// <param name="index">Integer that represents the index in the data at which copying begins</param>
+        /// <param name="length">Integer that represents the number of elements to copy</param>
+        /// <returns>System.Array containing the copied elements</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentException"></exception>
         public static int[] SubArray(this int[] data, int index, int length)
         {
             if(data == null)
@@ -21,7 +33,13 @@ namespace Task1
             return result;
         }
 
-       public static int[] MergeSort(int[] arr)
+        /// <summary>
+        /// Sorts the System.Array  
+        /// </summary>
+        /// <param name="arr">The one-dimensional System.Array for sorting</param>
+        /// <returns> The sorted System.Array </returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static int[] MergeSort(int[] arr)
         {
             if(arr == null)
             {
@@ -39,7 +57,14 @@ namespace Task1
             return Merge(MergeSort(arr.SubArray(0, mid_point)),MergeSort(arr.SubArray(mid_point,dif)));
         }
 
-       public static int[] Merge(int[] arr1, int[] arr2)
+        /// <summary>
+        /// Merges two sorted arrays into one 
+        /// </summary>
+        /// <param name="arr1">The first System.Array to merge</param>
+        /// <param name="arr2">The second System.Array to merge</param>
+        /// <returns> The sorted System.Array consisting of elements of two initial arrays</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        private static int[] Merge(int[] arr1, int[] arr2)
         {
             if(arr1 == null || arr2 == null)
             {
